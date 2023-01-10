0f8b5fea33a64900c1981fb0c6e28361ef4d4b35
derHerrMannelig <110530837+derHerrMannelig@users.noreply.github.com>
2023-01-10 11:30:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
-import requests
+import os
+import discord
 from discord.ext import commands
-from musicCog import musicCog
+from miscCog import miscCog
+from adminCog import adminCog
+# from musicCog import musicCog
+from keepAlive import keep_alive
 
-bot = commands.Bot(command_prefix= ['k!','K!'], case_insensitive=True)
+intents = discord.Intents.all() # Bot requires full Administrator privileges.
+bot = commands.Bot(command_prefix=['k!', 'K!'], case_insensitive=True, intents=intents)
 bot.remove_command('help')
-
-def get_cat():
-  kitty = requests.get('http://thecatapi.com/api/images/get.php')
-  if kitty.status_code == 200:
-      kitty = kitty.url
-      return kitty
-  else:
-      return 'TheCatApi is down, try again later!'
-
-bot.add_cog(musicCog(bot))
+# NOTE — if you want to host this bot locally — cut and paste add_cogs here w/o await statement.
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is online!')
- 
+    await bot.change_presence(activity=discord.Game('k!help'))
+    await bot.add_cog(miscCog(bot))
+    await bot.add_cog(adminCog(bot))
+  # await bot.add_cog(musicCog(bot))
+
 @bot.command()
 async def help(ctx):
-  await ctx.send("List of commands: \
-  \n`k!cat` — posts random cat image (powered by TheCatApi)")    
+    await ctx.send(
+"""**List of commands:**
+`k!cat` — posts random cat image and quote (powered by Some Random Api).
+`k!roll XdY` — rolls a dice, `X` - number of dice, `Y` - number of sides (max X & Y 20).
+`k!question *question*` — posts random answer.
+`k!user @user` — displays info about mentioned guild member (w/o mention displays your member info).
+`k!avatar @user` — extracts profile picture of the specified member.
+`k!say *message*` — repeats your text message.
+`k!bonk @user` — bomks umser. Yemp.
+**List of moderation commands (specific permissions required):**
+`k!kick @user *reason*` — kicks user and sends kick reason in DM.
+`k!ban @user *reason*` — bans user and sends ban reason in DM.
+`k!clear *amount*` — clears specific amount of messages (range 3-100).
+**List of features:**
+This bot greets newcomers.
+This bot indicates edited messages and displays changes.
+This bot indicates deleted messages and posts them.
+*Try mentioning Kotan in your discussion — and it will react!*"""
+    )
 
-@bot.command()
-async def cat(ctx):
-  cat = get_cat()
-  await ctx.send(cat)
-
-bot.run('TOKEN')
+keep_alive()
+secret = os.environ['TOKEN']
+bot.run(secret)
